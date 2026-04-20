aadaacc365c3a0679772b6d86428ba7e326bec81
Ashraful Islam <ashrafdcc1502@gmail.com>
2026-04-20 11:58:04 +0600

    notebook done

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='HealthGPT-RAG',
+    version='0.1',
+    packages=find_packages(),
+    author="Ashraful Islam",
+    description='A Retrieval-Augmented Generation (RAG) system for healthcare applications using LangChain and Pinecone.',
+    author_email="ashrafdcc1502@gmail.com",
+
+
+)
